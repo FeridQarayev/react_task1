02cb7a60466522b95611e283f2513d94724f1081
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-23 03:29:40 -0800

    create navbar

--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import styled from "./navbar.module.scss"
+import React from "react";
+import styled from "./navbar.module.scss";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className={styled.navbar}>
+      <div className={styled.navbar__content}>
+        <div className={styled.navbar__content__left}>
+          <p>Notary</p>
+        </div>
+        <div className={styled.navbar__content__right}>
+          <a href="#1">Home</a>
+          <a href="#2">Practice Areas</a>
+          <a href="#3">Testimonials</a>
+          <a href="#4">About</a>
+          <a href="#5">Contact</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
