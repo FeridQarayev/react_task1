ab2b083b6b3f1b2b91292053bca8d36c0757ad62
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-23 05:30:28 -0800

    add home content

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -19,6 +19,107 @@ function Home() {
           </p>
         </div>
       </div>
+
+      <div className={styled.home__content}>
+        <div className={styled.home__content__body}>
+          <div className={styled.home__content__body__title}>
+            <h1>
+              Practice <span>Areas</span>
+            </h1>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum,
+              commodi?
+            </p>
+          </div>
+          <div className={styled.home__content__body__cards}>
+            <div className={styled.home__content__body__cards__card}>
+              <div className={styled.home__content__body__cards__card__content}>
+                <img
+                  src="https://media.istockphoto.com/id/900791430/vector/bank-building-isolated-on-white-background-vector-illustration-flat-style.jpg?s=612x612&w=0&k=20&c=jc8wpyoZZ3I1qnz8xEfKf-P-dry-SqJyeaZkIEWOl34="
+                  alt="bank"
+                />
+                <h3>Bankruptey Law</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Odit, eum? Nobis illum eos reprehenderit, modi expedita quam
+                  voluptatum.
+                </p>
+              </div>
+            </div>
+            <div className={styled.home__content__body__cards__card}>
+              <div className={styled.home__content__body__cards__card__content}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/45/45091.png"
+                  alt="bank"
+                />
+                <h3>Bankruptey Law</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Odit, eum? Nobis illum eos reprehenderit, modi expedita quam
+                  voluptatum.
+                </p>
+              </div>
+            </div>
+            <div className={styled.home__content__body__cards__card}>
+              <div className={styled.home__content__body__cards__card__content}>
+                <img
+                  src="https://cdn4.vectorstock.com/i/1000x1000/94/53/hand-money-business-woman-take-dollars-buying-vector-28149453.jpg"
+                  alt="bank"
+                />
+                <h3>Bankruptey Law</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Odit, eum? Nobis illum eos reprehenderit, modi expedita quam
+                  voluptatum.
+                </p>
+              </div>
+            </div>
+            <div className={styled.home__content__body__cards__card}>
+              <div className={styled.home__content__body__cards__card__content}>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSj3naXX6uFGf9HhnLm9_lFbiC-ayfgKtgfSMlaoT85SrihCVTgUKoN5bmTCY8PjTfYHY&usqp=CAU"
+                  alt="bank"
+                />
+                <h3>Bankruptey Law</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Odit, eum? Nobis illum eos reprehenderit, modi expedita quam
+                  voluptatum.
+                </p>
+              </div>
+            </div>
+            <div className={styled.home__content__body__cards__card}>
+              <div className={styled.home__content__body__cards__card__content}>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXeo8gin5goNUd4wyWODIvYPchIppVfTFzpHzj5EnGUeZXIvmwK37NnPZCKlAFH927dkc&usqp=CAU"
+                  alt="bank"
+                />
+                <h3>Bankruptey Law</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Odit, eum? Nobis illum eos reprehenderit, modi expedita quam
+                  voluptatum.
+                </p>
+              </div>
+            </div>
+            <div className={styled.home__content__body__cards__card}>
+              <div className={styled.home__content__body__cards__card__content}>
+                <img
+                  src="https://t4.ftcdn.net/jpg/02/02/23/29/360_F_202232908_DHnVu2uRw7ValxiPBDpWbyJu5ZuiXnXb.jpg"
+                  alt="bank"
+                />
+                <h3>Bankruptey Law</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Odit, eum? Nobis illum eos reprehenderit, modi expedita quam
+                  voluptatum.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
